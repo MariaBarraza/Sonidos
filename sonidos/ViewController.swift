173e53287a4a8cf88790c7330fcd01365b5c958c
Alumno <ulsa@iMacD105.local>
2019-08-21 14:42:36 -0700

    Reproducir sonido

--- sonidos/ViewController.swift
+++ sonidos/ViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBAction func doTapReproducir(_ sender: Any) {
+        if player != nil {
+            player?.play()
+        }
+    }
+    //signo de interrogación para decir que no se esta seguro del tipo
+    var player : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //bundle es una clase qu representa el empaquetado de la aplicación, nil porque no se ocupa saber el tipo de dato que es
+        let path = Bundle.main.path(forResource: "Jumping-sound-effect.mp3", ofType: nil)
+        let url = URL(fileURLWithPath: path!)
+        
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+        }catch {
+            
+        }
+       
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
